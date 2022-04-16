fa1d39718274c194a31958b32b4d6285753c83c5
ysaito-2000 <saitoyukisaito@gmail.com>
2022-04-16 13:19:50 -0700

    scroll view scrolling fix

--- pages/Information/Info.js
+++ pages/Information/Info.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { View, Text, Image } from "react-native";
+import { View, Text, Image, Dimensions } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import {
   ScrollView,
@@ -22,10 +22,12 @@ const Info = (props) => {
   };
 
   return (
-    <SafeAreaView styles={{ flex: 1 }}>
+    <View styles={{ flex: 1 }}>
       <ScrollView
-        style={{ height: 500 }}
-        // contentContainerStyle={styles.container}
+        style={{
+          width: "100%",
+        }}
+        contentContainerStyle={styles.container}
       >
         <View style={styles.header}>
           <TouchableOpacity onPress={psuedoHome} style={styles.back}>
@@ -48,53 +50,98 @@ const Info = (props) => {
             </TouchableOpacity>
           </View>
         </View>
-        <View style={styles.contentItem}>
-          <Text>Total parking spots</Text>
-          <Text>
-            General ----------{" "}
-            <Text style={{ fontWeight: "bold" }}> 1,445</Text>
-          </Text>
-          <Text>
-            Disabled --------- <Text style={{ fontWeight: "bold" }}>8 </Text>
-          </Text>
-          <Text>
-            Employee -------- <Text style={{ fontWeight: "bold" }}>335</Text>
-          </Text>
-          <Text>
-            R-Permit --------- <Text style={{ fontWeight: "bold" }}>7</Text>
-          </Text>
-          <Text>
-            30-Min Time Zone --------{" "}
-            <Text style={{ fontWeight: "bold" }}>22</Text>
-          </Text>
+
+        <View
+          style={{
+            backgroundColor: "white",
+            borderRadius: 15,
+            width: "85%",
+            marginTop: "5%",
+            padding: 10,
+            flexDirection: "column",
+            alignItems: "center",
+          }}
+        >
+          <Text style={styles.contentItemTitle}>Total parking spots</Text>
+          <View style={{ width: "80%" }}>
+            <Text style={{ marginTop: 5, fontSize: 18 }}>
+              General ----------{" "}
+              <Text style={{ fontWeight: "bold" }}> 1,445</Text>
+            </Text>
+            <Text style={{ marginTop: 5, fontSize: 18 }}>
+              Disabled --------- <Text style={{ fontWeight: "bold" }}>8 </Text>
+            </Text>
+            <Text style={{ marginTop: 5, fontSize: 18 }}>
+              Employee -------- <Text style={{ fontWeight: "bold" }}>335</Text>
+            </Text>
+            <Text style={{ marginTop: 5, fontSize: 18 }}>
+              R-Permit --------- <Text style={{ fontWeight: "bold" }}>7</Text>
+            </Text>
+            <Text style={{ marginTop: 5, fontSize: 18 }}>
+              30-Min Time Zone --------{" "}
+              <Text style={{ fontWeight: "bold" }}>22</Text>
+            </Text>
+          </View>
         </View>
 
         <View style={styles.contentItem}>
-          <Text>Pricing</Text>
-          <Text style={{ fontWeight: "bold" }}>Mon - Fri</Text>
-          <Text>30 Mins --------- $1.00</Text>
-          <Text>24 Hours --------- $8.00</Text>
-          <Text>After 5:30 PM ------ $5.00</Text>
-          <Text style={{ fontWeight: "bold" }}>Sat - Sun</Text>
-          <Text>24 Hours --------- $5.00</Text>
-          <Text style={{ fontWeight: "bold" }}>No Change & Refund</Text>
+          <Text style={styles.contentItemTitle}>Pricing</Text>
+          <View style={{ width: "80%" }}>
+            <Text style={{ fontWeight: "bold", marginTop: 5, fontSize: 18 }}>
+              Mon - Fri
+            </Text>
+            <Text style={{ marginTop: 5, fontSize: 18 }}>
+              30 Mins --------- $1.00
+            </Text>
+            <Text style={{ marginTop: 5, fontSize: 18 }}>
+              24 Hours --------- $8.00
+            </Text>
+            <Text style={{ marginTop: 5, fontSize: 18 }}>
+              After 5:30 PM ------ $5.00
+            </Text>
+            <Text style={{ fontWeight: "bold", marginTop: 5, fontSize: 18 }}>
+              Sat - Sun
+            </Text>
+            <Text style={{ marginTop: 5, fontSize: 18 }}>
+              24 Hours --------- $5.00
+            </Text>
+            <Text style={{ fontWeight: "bold", marginTop: 5, fontSize: 18 }}>
+              No Change & Refund
+            </Text>
+          </View>
         </View>
 
-        <View style={styles.contentItem}>
-          <Text>Hours</Text>
-          <Text style={{ fontWeight: "bold" }}>Mon - Fri</Text>
-          <Text>Open 6:00 Am - 1:00 AM</Text>
-          <Text style={{ fontWeight: "bold" }}>
-            'E', 'R', 'H', Carpool, Overnight
-          </Text>
-          <Text style={{ fontWeight: "bold" }}>Permits</Text>
-          <Text>Open 24 Hours</Text>
-          <Text style={{ fontWeight: "bold" }}>Pay Method</Text>
-          <Text> 1. ParkMobile App</Text>
-          <Text> 2. Pay Station on Each Parking Level</Text>
+        <View style={[styles.contentItem, { marginBottom: 20 }]}>
+          <Text style={styles.contentItemTitle}>Hours</Text>
+          <View style={{ width: "80%" }}>
+            <Text style={{ fontWeight: "bold", marginTop: 5, fontSize: 18 }}>
+              Mon - Fri
+            </Text>
+            <Text style={{ fontWeight: "bold", marginTop: 5, fontSize: 18 }}>
+              Open 6:00 Am - 1:00 AM
+            </Text>
+            <Text style={{ fontWeight: "bold", marginTop: 5, fontSize: 18 }}>
+              'E', 'R', 'H', Carpool, Overnight
+            </Text>
+            <Text style={{ fontWeight: "bold", marginTop: 5, fontSize: 18 }}>
+              Permits
+            </Text>
+            <Text style={{ marginTop: 5, fontSize: 18 }}>Open 24 Hours</Text>
+            <Text style={{ fontWeight: "bold", marginTop: 5, fontSize: 18 }}>
+              Pay Method
+            </Text>
+            <Text style={{ marginTop: 5, fontSize: 18 }}>
+              {" "}
+              1. ParkMobile App
+            </Text>
+            <Text style={{ marginTop: 5, fontSize: 18 }}>
+              {" "}
+              2. Pay Station on Each Parking Level
+            </Text>
+          </View>
         </View>
       </ScrollView>
-    </SafeAreaView>
+    </View>
   );
 };
 
